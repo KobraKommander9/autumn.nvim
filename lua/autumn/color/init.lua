9cdc43235761728ed5f5eb47c52e62967ea2c53f
Ed <ed.kilgore@tcnbroadcasting.com>
2024-09-11 17:27:09 -0600

    fix color generation

--- lua/autumn/color/init.lua
+++ lua/autumn/color/init.lua
@@ -2,29 +2,25 @@ local hsl = require("autumn.color.hsl")
 
 local M = {}
 
-M.__index = function(self, key)
-	if self[key] then
-		return self[key]
-	end
-
-	return self.base[key]
-end
-
-M.__call = function(self)
-	return self.base
-end
-
 function M.new(base, bright, dim)
-	local self = setmetatable({}, M)
+	local c = setmetatable({}, {
+		__index = function(self, key)
+			if self[key] then
+				return self[key]
+			end
+
+			return self.base[key]
+		end,
+	})
 
 	bright = bright or 50
 	dim = dim or 50
 
-	self.base = hsl(base)
-	self.bright = type(bright) == "number" and self.base.lighten(bright) or hsl(bright)
-	self.dim = type(dim) == "number" and self.base.darken(dim) or hsl(dim)
+	c.base = hsl(base)
+	c.bright = type(bright) == "number" and c.base.lighten(bright) or hsl(bright)
+	c.dim = type(dim) == "number" and c.base.darken(dim) or hsl(dim)
 
-	return self
+	return c
 end
 
 return M
