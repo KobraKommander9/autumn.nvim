bdd8a38b5abc3a05c00f426bd25c86d26f531dba
Ed <ed.kilgore@tcnbroadcasting.com>
2024-09-20 14:42:16 -0600

    update color groups

--- lua/autumn/group/editor.lua
+++ lua/autumn/group/editor.lua
@@ -1,84 +1,85 @@
 local M = {}
 
 function M.get(spec, _)
-  local diag = spec.diag
-  local diff = spec.diff
+	local diag = spec.diag
+	local diff = spec.diff
+	local syn = spec.syntax
 
-  return {
-    Normal = { bg = spec.bg1, fg = spec.fg1 },
-    NormalFloat = { bg = spec.bg0 },
-    NormalNC = { link = "Normal" },
-    Visual = { bg = spec.bg3 },
-    VisualNOS = { link = "Visual" },
+	return {
+		Normal = { bg = spec.bg1, fg = spec.fg1 },
+		NormalFloat = { bg = spec.bg0 },
+		NormalNC = { link = "Normal" },
+		Visual = { bg = spec.bg3 },
+		VisualNOS = { link = "Visual" },
 
-    EndOfBuffer = { fg = spec.bg1 },
-    Title = { fg = spec.fg1, style = "bold" },
-    Whitespace = { fg = spec.bg3 },
-    NonText = { fg = spec.bg3 },
-    Conceal = { link = "NonText" },
-    SpecialKey = { link = "NonText" },
+		EndOfBuffer = { fg = spec.bg1 },
+		Title = { fg = spec.fg1, style = "bold" },
+		Whitespace = { fg = spec.bg3 },
+		NonText = { fg = spec.bg3 },
+		Conceal = { link = "NonText" },
+		SpecialKey = { link = "NonText" },
 
-    CursorLine = { bg = spec.bg2 },
-    CursorColumn = { link = "CursorLine" },
-    CursorLineNr = { fg = spec.p2, style = "bold" },
-    LineNr = { fg = spec.fg3 },
-    ColorColumn = { bg = spec.bg3 },
+		CursorLine = { bg = spec.bg2 },
+		CursorColumn = { link = "CursorLine" },
+		CursorLineNr = { fg = spec.p2, style = "bold" },
+		LineNr = { fg = spec.fg3 },
+		ColorColumn = { bg = spec.bg3 },
 
-    Cursor = { fg = spec.bg1, bg = spec.fg1 },
-    lCursor = { link = "Cursor" },
-    CursorIM = { link = "Cursor" },
+		Cursor = { fg = spec.bg1, bg = spec.fg1 },
+		lCursor = { link = "Cursor" },
+		CursorIM = { link = "Cursor" },
 
-    SignColumn = { fg = spec.fg3 },
-    SignColumnSB = { link = "SignColumn" },
+		SignColumn = { fg = spec.fg3 },
+		SignColumnSB = { link = "SignColumn" },
 
-    FoldColumn = { fg = spec.fg3 },
-    Folded = { fg = spec.fg3, bg = spec.bg2 },
+		FoldColumn = { fg = spec.fg3 },
+		Folded = { fg = spec.fg3, bg = spec.bg2 },
 
-    StatusLine = { fg = spec.bg2, bg = spec.fg2 },
-    StatusLineNC = { fg = spec.fg0, bg = spec.bg0 },
-    TabLine = { fg = spec.fg2, bg = spec.bg2 },
-    TabLineFill = { bg = spec.bg0 },
-    TabLineSel = { fg = spec.bg3, bg = spec.fg3 },
+		StatusLine = { fg = spec.bg2, bg = spec.fg2 },
+		StatusLineNC = { fg = spec.fg0, bg = spec.bg0 },
+		TabLine = { fg = spec.fg2, bg = spec.bg2 },
+		TabLineFill = { bg = spec.bg0 },
+		TabLineSel = { fg = spec.bg3, bg = spec.fg3 },
 
-    WinSeparator = { link = "Normal" },
-    WinBarNC = { bg = spec.bg0, fg = spec.fg3 },
-    WinBar = { bg = spec.bg0, fg = spec.fg3, style = "bold" },
-    VertSplit = { link = "WinSeparator" },
+		WinSeparator = { link = "Normal" },
+		WinBarNC = { bg = spec.bg0, fg = spec.fg3 },
+		WinBar = { bg = spec.bg0, fg = spec.fg3, style = "bold" },
+		VertSplit = { link = "WinSeparator" },
 
-    FloatBorder = { fg = spec.fg1 },
+		FloatBorder = { fg = spec.fg1 },
 
-    Search = { bg = spec.sel0, fg = spec.bg1 },
-    Substitute = { bg = diag.error, fg = spec.bg1 },
-    IncSearch = { bg = diag.info, fg = spec.bg1 },
-    CurSearch = { link = "IncSearch" },
+		Search = { bg = spec.sel0, fg = spec.bg1 },
+		Substitute = { bg = diag.error, fg = spec.bg1 },
+		IncSearch = { bg = spec.sel1, fg = spec.bg1 },
+		CurSearch = { link = "IncSearch" },
 
-    SpellBad = { sp = diag.error, style = "undercurl" },
-    SpellCap = { sp = diag.warn, style = "undercurl" },
-    SpellLocal = { sp = diag.info, style = "undercurl" },
-    SpellRare = { sp = diag.info, style = "undercurl" },
+		SpellBad = { sp = diag.error, style = "undercurl" },
+		SpellCap = { sp = diag.warn, style = "undercurl" },
+		SpellLocal = { sp = diag.info, style = "undercurl" },
+		SpellRare = { sp = diag.info, style = "undercurl" },
 
-    DiffAdd = { bg = diff.add },
-    DiffChange = { bg = diff.change },
-    DiffDelete = { bg = diff.delete },
-    DiffText = { bg = diff.text },
+		DiffAdd = { bg = diff.add },
+		DiffChange = { bg = diff.change },
+		DiffDelete = { bg = diff.delete },
+		DiffText = { bg = diff.text },
 
-    Pmenu = { bg = spec.bg2 },
-    PmenuSbar = { link = "Pmenu" },
-    PmenuSel = { bg = spec.fg2, fg = spec.bg2 },
-    PmenuThumb = { bg = spec.bg3 },
-    WildMenu = { link = "Pmenu" },
+		Pmenu = { bg = spec.bg2 },
+		PmenuSbar = { link = "Pmenu" },
+		PmenuSel = { bg = spec.fg2, fg = spec.bg2 },
+		PmenuThumb = { bg = spec.bg3 },
+		WildMenu = { link = "Pmenu" },
 
-    MsgArea = { fg = spec.fg2 },
-    ModeMsg = { fg = diag.ok, style = "bold" },
-    MoreMsg = { fg = diag.info, style = "bold" },
-    Question = { link = "MoreMsg" },
-    ErrorMsg = { fg = diag.error },
-    WarningMsg = { fg = diag.warn },
+		MsgArea = { fg = spec.fg2 },
+		ModeMsg = { fg = diag.ok, style = "bold" },
+		MoreMsg = { fg = diag.info, style = "bold" },
+		Question = { link = "MoreMsg" },
+		ErrorMsg = { fg = diag.error },
+		WarningMsg = { fg = diag.warn },
 
-    QuickFixLine = { bg = diag.info, fg = spec.bg2 },
-    MatchParen = { fg = diag.warn, style = "bold" },
-    Directory = { fg = spec.p1 },
-  }
+		QuickFixLine = { bg = diag.info, fg = spec.bg2 },
+		MatchParen = { fg = diag.warn, style = "bold" },
+		Directory = { fg = syn.preproc },
+	}
 end
 
 return M
--- lua/autumn/palette/autumn.lua
+++ lua/autumn/palette/autumn.lua
@@ -2,25 +2,25 @@ local Color = require("autumn.color")
 local colors = require("autumn.palette.colors")
 
 local meta = {
-  name = "autumn",
-  light = false,
+	name = "autumn",
+	light = false,
 }
 
 local palette = colors({
-  white = Color.new("#f4ebbe"),
-  black = Color.new("#272d2d"),
-  red = Color.new("#800e13"),
-  orange = Color.new("#f27f34"),
-  yellow = Color.new("#edd83d"),
-  green = Color.new("#799431"),
-  blue = Color.new("#1c77c3"),
-  purple = Color.new("#846b8a", 30),
-  pink = Color.new("#d65780", 10),
-  magenta = Color.new("#a23b72", 40),
-  cyan = Color.new("#4bc6b9"),
+	white = Color.new("#f4ebbe"),
+	black = Color.new("#272d2d"),
+	red = Color.new("#800e13"),
+	orange = Color.new("#f27f34"),
+	yellow = Color.new("#edd83d"),
+	green = Color.new("#799431"),
+	blue = Color.new("#1c77c3"),
+	purple = Color.new("#846b8a", 30),
+	pink = Color.new("#d65780", 10),
+	magenta = Color.new("#a23b72", 40),
+	cyan = Color.new("#4bc6b9"),
 
-  primary = Color.new("#eb5e28"),
-  secondary = Color.new("#6a8532"),
+	primary = Color.new("#eb5e28"),
+	secondary = Color.new("#6a8532"),
 })
 
 palette.gray = Color.new(palette.white.mix(palette.black, 90))
@@ -44,79 +44,80 @@ palette.secondary1 = palette.secondary.lighten(20).hex
 palette.secondary2 = palette.secondary.lighten(40).hex
 
 local function generate_spec(p)
-  local spec = {
-    bg0 = p.bg0,
-    bg1 = p.bg1,
-    bg2 = p.bg2,
-    bg3 = p.bg3,
+	local spec = {
+		bg0 = p.bg0,
+		bg1 = p.bg1,
+		bg2 = p.bg2,
+		bg3 = p.bg3,
 
-    fg0 = p.fg0,
-    fg1 = p.fg1,
-    fg2 = p.fg2,
-    fg3 = p.fg3,
+		fg0 = p.fg0,
+		fg1 = p.fg1,
+		fg2 = p.fg2,
+		fg3 = p.fg3,
 
-    p0 = p.primary.base,
-    p1 = p.primary1,
-    p2 = p.primary2,
+		p0 = p.primary.base,
+		p1 = p.primary1,
+		p2 = p.primary2,
 
-    s0 = p.secondary.base,
-    s1 = p.secondary1,
-    s2 = p.secondary2,
+		s0 = p.secondary.base,
+		s1 = p.secondary1,
+		s2 = p.secondary2,
 
-    sel0 = p.gray.bright,
-  }
+		sel0 = p.gray.bright,
+		sel1 = p.orange.base,
+	}
 
-  spec.syntax = {
-    bracket = p.white.base,
-    builtin0 = p.pink.bright,    -- builtin variable
-    builtin1 = spec.p1,          -- builtin type
-    builtin2 = p.magenta.bright, -- builtin const
-    comment = p.comment,
-    conditional = spec.p1,       -- conditional and loop
-    const = p.magenta.bright,    -- constants, imports, boolean
-    dep = spec.fg3,              -- deprecated
-    field = p.orange.base,
-    func = spec.s2,
-    ident = spec.s1,
-    keyword = spec.p2,
-    number = p.magenta.bright,
-    operator = spec.p2,
-    preproc = spec.p0,
-    regex = p.blue.bright,
-    statement = spec.p2,
-    string = p.green.bright,
-    type = spec.p1,
-    variable = p.white.base,
-  }
+	spec.syntax = {
+		-- bracket = p.white.base,
+		-- builtin0 = p.pink.bright,    -- builtin variable
+		-- builtin1 = spec.p1,          -- builtin type
+		-- builtin2 = p.magenta.bright, -- builtin const
+		comment = p.comment,
+		conditional = spec.p1, -- conditional and loop
+		const = p.magenta.bright, -- constants, imports, boolean
+		-- dep = spec.fg3,              -- deprecated
+		-- field = p.orange.base,
+		func = spec.s2,
+		ident = p.soft_pink.base,
+		keyword = spec.p2,
+		number = p.soft_red.bright,
+		operator = p.soft_cyan.base,
+		preproc = p.hard_red.bright,
+		-- regex = p.blue.bright,
+		-- statement = spec.p2,
+		string = p.green.bright,
+		type = spec.p1,
+		-- variable = p.white.base,
+	}
 
-  spec.diag = {
-    error = p.red.bright,
-    warn = p.yellow.bright,
-    info = p.orange.bright,
-    hint = p.blue.bright,
-    ok = p.green.bright,
-  }
+	spec.diag = {
+		error = p.red.bright,
+		warn = p.yellow.bright,
+		info = p.blue.bright,
+		hint = p.purple.bright,
+		ok = p.green.bright,
+	}
 
-  spec.diff = {
-    add = p.green.base,
-    delete = p.red.bright,
-    change = p.blue.bright,
-    text = p.cyan.dim,
-  }
+	spec.diff = {
+		add = p.green.base,
+		delete = p.red.bright,
+		change = p.blue.bright,
+		text = p.cyan.dim,
+	}
 
-  spec.git = {
-    add = p.green.base,
-    removed = p.red.bright,
-    changed = p.blue.bright,
-    conflict = p.orange.base,
-    ignored = p.comment,
-  }
+	spec.git = {
+		add = p.green.base,
+		removed = p.red.bright,
+		changed = p.blue.bright,
+		conflict = p.orange.bright,
+		ignored = p.comment,
+	}
 
-  return spec
+	return spec
 end
 
 return {
-  meta = meta,
-  palette = palette,
-  generate_spec = generate_spec,
+	meta = meta,
+	palette = palette,
+	generate_spec = generate_spec,
 }
