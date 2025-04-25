ef60e0b5df2fa241438029d50af8df413232da9a
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-24 22:50:47 -0600

    add builtin1 color

--- lua/autumn/palette.lua
+++ lua/autumn/palette.lua
@@ -80,7 +80,7 @@ local function generate_spec(p)
 	spec.syntax = {
 		bracket = p.white.hex, -- brackets and punctuation
 		builtin0 = spec.p0, -- builtin variable
-		builtin1 = spec.p0, -- builtin type
+		builtin1 = spec.s0, -- builtin type
 		builtin2 = spec.p0, -- builtin const
 		builtin3 = spec.p0, -- not used
 		comment = p.comment.hex, -- comment
