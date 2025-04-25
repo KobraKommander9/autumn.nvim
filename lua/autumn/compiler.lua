78735b9ce75c7092986403d2a6a9ea9c990ff0af
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-24 22:03:30 -0600

    remove log

--- lua/autumn/compiler.lua
+++ lua/autumn/compiler.lua
@@ -116,8 +116,6 @@ local function inspect(tbl, color_names)
 	color_names = color_names or {}
 	local list = {}
 
-	vim.notify(vim.inspect(color_names))
-
 	for k, v in pairs(tbl) do
 		if type(v) == "string" and color_names[v] then
 			table.insert(list, fmt("%s = %s", k, color_names[v]))
