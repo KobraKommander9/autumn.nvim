2d0cdd850bd213b2abe6af667571f5f2aa098eb9
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-24 22:48:18 -0600

    add variable color

--- lua/autumn/palette.lua
+++ lua/autumn/palette.lua
@@ -98,7 +98,7 @@ local function generate_spec(p)
 		statement = spec.p0, -- statements
 		string = spec.p0, -- strings
 		type = spec.p0, -- types
-		variable = p.white_dim.hex, -- variables
+		variable = p.white.dim.hex, -- variables
 		-- bracket = spec.fg2, -- brackets and punctuation
 		-- builtin0 = p.red.hex, -- builtin variable
 		-- builtin1 = spec.p0, -- builtin type
