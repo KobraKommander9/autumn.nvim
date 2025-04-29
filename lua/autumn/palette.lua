2ad0b9322c752055bab4e505428888f360f36d99
Ed <ed.kilgore@tcnbroadcasting.com>
2025-04-29 10:46:06 -0600

    use off green for stuff

--- lua/autumn/palette.lua
+++ lua/autumn/palette.lua
@@ -40,16 +40,22 @@ palette.gray = make(
 	palette.white.bright.mix(palette.black.bright, 90)
 )
 
-palette.bg0 = make(palette.gray.darken(60))
-palette.bg1 = make(palette.gray.darken(50))
-palette.bg2 = make(palette.gray.darken(20))
-palette.bg3 = make(palette.gray.lighten(10))
-palette.bg4 = make(palette.gray.lighten(20))
+local off_green = make(
+	palette.gray.mix(palette.secondary, 50),
+	palette.gray.dim.mix(palette.secondary.dim, 50),
+	palette.gray.bright.mix(palette.secondary.bright, 50)
+)
 
-palette.fg0 = make(palette.gray.lighten(90))
-palette.fg1 = make(palette.gray.lighten(80))
-palette.fg2 = make(palette.gray.lighten(70))
-palette.fg3 = make(palette.gray.lighten(60))
+palette.bg0 = make(off_green.darken(60))
+palette.bg1 = make(off_green.darken(50))
+palette.bg2 = make(off_green.darken(20))
+palette.bg3 = make(off_green.lighten(10))
+palette.bg4 = make(off_green.lighten(20))
+
+palette.fg0 = make(off_green.lighten(90))
+palette.fg1 = make(off_green.lighten(80))
+palette.fg2 = make(off_green.lighten(70))
+palette.fg3 = make(off_green.lighten(60))
 
 palette.comment = make(palette.bg1.mix(palette.fg1, 40))
 
