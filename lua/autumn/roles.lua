238e2da1ee8ec9e06c1d4057bc5a039652cd51a4
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-21 15:15:13 -0500

    update roles

--- lua/autumn/roles.lua
+++ lua/autumn/roles.lua
@@ -11,193 +11,149 @@ function M.get(p, styles)
 
 	roles.ui = {
 		bg = {
-			base = p.bg0,
-			raised = p.bg1,
-			panel = p.bg2,
-			selection = p.bg3,
+			main = p.bg0, -- standard editor background
+			alt = p.bg1, -- sidebar / inactive windows
+			float = p.bg2, -- popus, hover docs
+			accent = p.bg3, -- high constrast (folds, visual search)
 		},
 
 		fg = {
-			normal = p.fg1,
-			dim = p.fg0,
-			subtle = p.fg3,
-			cool = p.fg2,
+			main = p.fg1, -- primary text
+			dim = p.fg0, -- secondary/description text
+			subtle = p.fg3, -- hidden/non-text (whitespaces/ eob)
+		},
+
+		selection = {
+			bg = p.sel0,
+			active = p.sel1, -- focused selection or "current match"
 		},
 
 		border = {
-			normal = p.border,
+			base = p.border,
 			focus = p.sel1,
 		},
-	}
 
-	roles.emphasis = {
-		high = {
-			fg = p.primary.base,
-			style = get_style(styles.emphasis_high, "bold"),
-		},
-
-		medium = {
-			fg = p.primary.dim,
-			style = get_style(styles.emphasis_medium),
-		},
-
-		low = {
-			fg = p.fg3,
-			style = get_style(styles.emphasis_low),
-		},
-
-		muted = {
-			fg = p.fg0,
-			style = get_style(styles.emphasis_muted, "italic"),
+		gutter = {
+			bg = p.bg0,
+			fg = p.fg3, -- line numbers
+			active = p.fg0, -- current line number
 		},
 	}
 
-	roles.text = {
+	roles.syntax = {
+		builtin = {
+			fg = p.blue.base,
+			style = get_style(styles.builtins),
+		},
+
 		comment = {
 			fg = roles.ui.fg.subtle,
 			style = get_style(styles.comments, "italic"),
 		},
 
-		dim = {
+		constant = {
+			fg = p.purple.base,
+			style = get_style(styles.constants),
+		},
+
+		control = {
+			fg = p.primary.dim,
+			style = get_style(styles.flows),
+		},
+
+		field = {
+			fg = p.fg2,
+			style = get_style(styles.fields),
+		},
+
+		func = {
+			fg = p.secondary.base,
+			style = get_style(styles.functions),
+		},
+
+		keyword = {
+			fg = p.primary.base,
+			style = get_style(styles.keywords, "bold"),
+		},
+
+		operator = {
+			fg = p.primary.dim,
+			style = get_style(styles.operators),
+		},
+
+		statement = {
+			fg = p.primary.base,
+			style = get_style(styles.statements),
+		},
+
+		type = {
+			fg = p.secondary.base,
+			style = get_style(styles.types),
+		},
+
+		variable = {
+			fg = roles.ui.fg.main,
+			style = get_style(styles.variables),
+		},
+
+		-- data atoms
+		boolean = {
+			fg = p.purple.base,
+			style = get_style(styles.booleans),
+		},
+
+		number = {
+			fg = p.purple.base,
+			style = get_style(styles.numbers),
+		},
+
+		string = {
+			fg = p.cyan.base,
+			style = get_style(styles.strings),
+		},
+
+		-- punctuation
+		bracket = {
+			fg = roles.ui.fg.main,
+		},
+
+		punctuation = {
 			fg = roles.ui.fg.dim,
 		},
 
+		-- contracts / modules
+		container = {
+			interface = {
+				fg = p.magenta.dim,
+				style = get_style(styles.interfaces),
+			},
+
+			module = {
+				fg = p.magenta.base,
+				style = get_style(styles.modules),
+			},
+		},
+	}
+
+	roles.emphasis = {
+		high = { fg = p.primary.base, style = "bold" },
+		medium = { fg = p.primary.dim },
+		low = { fg = p.fg0 },
+		muted = { fg = p.fg3, style = "italic" },
+	}
+
+	roles.info = {
 		documentation = {
 			fg = roles.ui.fg.normal,
 			style = get_style(styles.documentation, "italic"),
 		},
 
-		normal = {
-			fg = roles.ui.fg.normal,
-		},
-
-		punctuation = {
-			fg = roles.ui.fg.normal,
-		},
-
-		subtle = {
-			fg = roles.ui.fg.subtle,
-		},
-
-		quote = {
-			fg = p.cyan.base,
-			style = get_style(styles.quotes, "italic"),
+		msg = {
+			fg = p.fg1,
 		},
 	}
 
-	roles.code = {
-		contract = {
-			interface = {
-				fg = p.magenta.dim,
-				style = get_style(styles.contracts),
-			},
-
-			module = {
-				fg = p.magenta.base,
-				style = get_style(styles.contracts),
-			},
-		},
-
-		control = {
-			flow = {
-				fg = p.primary.dim,
-				style = get_style(styles.flows),
-			},
-
-			keyword = {
-				fg = p.primary.base,
-				style = get_style(styles.keywords, "bold"),
-			},
-
-			operator = {
-				fg = p.primary.dim,
-				style = get_style(styles.operators),
-			},
-
-			statement = {
-				fg = p.primary.base,
-				style = get_style(styles.statements),
-			},
-		},
-
-		system = {
-			builtin = {
-				base = {
-					fg = p.blue.base,
-					style = get_style(styles.preprocs),
-				},
-
-				dim = {
-					fg = p.blue.dim,
-					style = get_style(styles.preprocs),
-				},
-			},
-
-			data = {
-				boolean = {
-					fg = p.purple.base,
-					style = get_style(styles.booleans),
-				},
-
-				constant = {
-					fg = p.purple.base,
-					style = get_style(styles.constants),
-				},
-
-				number = {
-					fg = p.purple.base,
-					style = get_style(styles.numbers),
-				},
-
-				string = {
-					fg = p.cyan.base,
-					style = get_style(styles.strings),
-				},
-			},
-		},
-
-		user = {
-			type = {
-				base = {
-					fg = p.secondary.base,
-					style = get_style(styles.types),
-				},
-
-				dim = {
-					fg = p.secondary.dim,
-					style = get_style(styles.types),
-				},
-			},
-
-			field = {
-				fg = roles.ui.normal,
-				style = get_style(styles.fields),
-			},
-
-			func = {
-				fg = p.secondary.base,
-				style = get_style(styles.functions),
-			},
-
-			variable = {
-				fg = roles.ui.fg.normal,
-				style = get_style(styles.variables),
-			},
-		},
-	}
-
-	roles.info = {
-		base = {
-			fg = p.blue.base,
-		},
-
-		link = {
-			fg = p.blue.base,
-			style = get_style(styles.links, "italic,underline"),
-		},
-	}
-
-	roles.diagnostics = {
+	roles.semantic = {
 		error = {
 			fg = p.red.base,
 			style = get_style(styles.diagnostics, "italic,underline"),
@@ -217,24 +173,41 @@ function M.get(p, styles)
 			fg = p.cyan.base,
 			style = get_style(styles.diagnostics, "italic,underline"),
 		},
+
+		changed = {
+			fg = p.yellow.base,
+		},
+
+		deleted = {
+			fg = p.red.base,
+		},
+
+		success = {
+			fg = p.green.base,
+		},
 	}
 
-	roles.diff = {
-		add = {
-			fg = p.green.dim,
+	roles.markup = {
+		bold = { style = "bold" },
+
+		heading = {
+			fg = p.primary.base,
+			style = "bold",
 		},
 
-		del = {
-			fg = p.red.dim,
+		italic = { style = "italic" },
+
+		link = {
+			fg = p.blue.base,
+			style = get_style(styles.links, "underline"),
 		},
 
-		change = {
-			fg = p.yellow.dim,
+		quote = {
+			fg = p.fg0,
+			style = "italic",
 		},
 
-		text = {
-			fg = p.primary.dim,
-		},
+		raw = { fg = p.cyan.base },
 	}
 
 	return roles
